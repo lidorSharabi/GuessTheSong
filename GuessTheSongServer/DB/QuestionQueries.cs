b17a8564e479fb4a5e0df6cc9b002dbb1040a9bc
karin haim <karin.haim@live.biu.ac.il>
2019-02-20 18:48:47 +0200

    added question queries

---
+++ GuessTheSongServer/DB/QuestionQueries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheSongServer.DB
+{
+    public static class QuestionQueries
+    {
+        public static string easyQuery = @"SELECT lyrics.song, lyrics.lyrics FROM guessthesong.artists 
+INNER JOIN guessthesong.lyrics
+ON artists.id = lyrics.artist
+INNER JOIN guessthesong.genres
+ON lyrics.genre = genres.id 
+WHERE genres.genre = '{0}'
+AND artists.filteredName='{1}'
+AND (lyrics.year >= '{2}' AND lyrics.year < '{3}')
+LIMIT 20";
+
+
+
+    }
+}
---
+++ GuessTheSongServer/DB/QuestionsManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheSongServer.DB
+{
+    public class QuestionsManager
+    {
+        private string artist;
+        private string genre;
+        private int year;
+        private int decadeStart;
+        private int decadeEnd;
+        private DataBaseHandler dbHandler;
+
+        public QuestionsManager(string artist, string genre, int year, DataBaseHandler dbHandler)
+        {
+            this.artist = artist;
+            this.genre = genre;
+            this.year = year;
+            decadeStart = (year / 10) * 10;
+            decadeEnd = decadeStart + 10;
+            this.dbHandler = dbHandler;
+        }
+
+        public List<Song> AskQuestion()
+        {
+            string query = String.Format(QuestionQueries.easyQuery, genre, artist, decadeStart, decadeEnd);
+            return dbHandler.GetSongs(query);
+        }
+    }
+}
